f88dff8bef57e9da5163330a99792c442e61670d
Laurent Tshisekedi <laurent.n.tshisekedi@gmail.com>
2023-08-27 22:51:27 -0500

    updated JS logic & updated HTML structure with button selections

--- script/RPS.js
+++ script/RPS.js
@@ -1,13 +1,64 @@
-function gameTime() {
-  const computerMove = Math.random();
 
-  let content = document.querySelector(".paragraphContent");
 
-  if (computerMove > 0 && computerMove <= 1 / 3) {
-    content.innerHTML = "Rock";
-  } else if (computerMove > 1 / 3 && computerMove <= 2 / 3) {
-    content.innerHTML = "Paper";
-  } else if (computerMove > 2 / 3) {
-    content.innerHTML = "Scissors";
+function playGame(playerMove) {
+  const computerPick = pickComputerMove();
+
+  let result = ""; 
+
+  if (playerMove === "Scissors") {
+    if (computerPick === "Rock") {
+      result = "You Lose."; 
+       
+    } else if (computerPick === "Paper") {
+      result = "You win."; 
+       
+    } else if (computerPick === "Scissors") {
+      result = "Tie."; 
+       
+    }
+    
+  } else if (playerMove === "Rock") {
+    if (computerPick === "Rock") {
+      result = "Tie."; 
+       
+    } else if (computerPick === "Paper") {
+      result = "You Lose."; 
+       
+    } else if (computerPick === "Scissors") {
+      result = "You win.";
+       
+    }
+  } else if (playerMove === "Paper") {
+    if (computerPick === "Rock") {
+      result = "You win."; 
+       
+    } else if (computerPick === "Paper") {
+      result = "Tie."; 
+       
+    } else if (computerPick === "Scissors") {
+  
+      result = "You Lose."; 
+    }
   }
+
+  document.querySelector(".js-move").innerHTML = `You picked ${playerMove}. Computer picked ${computerPick}. ${result}`; 
 }
+
+
+function pickComputerMove() {
+  const randomNumber = Math.random();
+
+  let computerMove = ""; 
+
+  if (randomNumber > 0 && randomNumber <= 1 / 3) {
+    computerMove = "Rock";
+  } else if (randomNumber > 1 / 3 && randomNumber <= 2 / 3) {
+    computerMove = "Paper";
+  } else if (randomNumber > 2 / 3) {
+    computerMove = "Scissors";
+  }; 
+
+  return computerMove; 
+}
+      
+
